89922725561150f888c6a6ce241d5c3c4a725c36
Kseniia <kseniiakatashova@gmail.com>
2022-11-10 23:02:57 -0500

    Implementation of encryption the user passwords

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -23,6 +24,29 @@ const userSchema = new mongoose.Schema({
         type: Date,
         default: Date.now()
     }
+});
+
+userSchema.pre("save", function (next) {
+    let user = this;
+
+    //Generate a unique SALT
+    bcrypt.genSalt(10)
+    .then(salt => {
+        //Hash the password using the generated SALT
+        bcrypt.hash(user.password, salt)
+        .then(hashedPwd => {
+            //Password was hashed.
+            //Update the user model before we save it to the database
+            user.password = hashedPwd;
+            next();
+        })
+        .catch(err => {
+            console.log(`Error occurred when hashing... ${error}`);
+        })
+    })
+    .catch(err => {
+        console.log(`Error occurred when salting... ${error}`);
+    });
 });
 
 const userModel = mongoose.model("users", userSchema)
